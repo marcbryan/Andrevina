e1a6aaa55733a0879f9399f8013b51de5dffba66
marcbryan <marcboakyeflores98@gmail.com>
2018-12-16 20:20:41 +0100

    Persistencia implementada

--- app/src/main/java/com/andrevina/et/andrevina/FameActivity.java
+++ app/src/main/java/com/andrevina/et/andrevina/FameActivity.java
@@ -7,6 +7,13 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -20,48 +27,77 @@ public class FameActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String str_intentos = intent.getStringExtra("EXTRA_INTENTOS");
         String player_Name = intent.getStringExtra("EXTRA_NAME");
-        Jugador jugador = new Jugador(player_Name, Integer.parseInt(str_intentos));
-        Jugador.jugadors.add(jugador);
-        String txt = "";
-        TextView textView = findViewById(R.id.textView);
 
-        //Ordena los jugadores por el numero de intentos mas bajo
-        Collections.sort(Jugador.jugadors, new Comparator<Jugador>() {
-            @Override
-            public int compare(Jugador j1, Jugador j2) {
-                return new Integer(j1.getIntentos()).compareTo(new Integer(j2.getIntentos()));
+        if (str_intentos != null && player_Name != null) {
+            addRecordToFile(player_Name, str_intentos);
+        }
+
+        if (Jugador.jugadors.size() != 0) {
+            //Ordena los jugadores por el numero de intentos mas bajo
+            Collections.sort(Jugador.jugadors, new Comparator<Jugador>() {
+                @Override
+                public int compare(Jugador j1, Jugador j2) {
+                    return new Integer(j1.getIntentos()).compareTo(new Integer(j2.getIntentos()));
+                }
+            });
+
+            //Coloca los jugadores que existan en la tabla de records
+            for (int i=0; i < Jugador.jugadors.size(); i++) {
+                LinearLayout linearLayout = new LinearLayout(this);
+                linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+                LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(0, 100);
+                lparams.weight = 1;
+
+                TextView nom_jug = new TextView(this);
+                nom_jug.setTextSize(20);
+                nom_jug.setText(Jugador.jugadors.get(i).getNombre());
+                nom_jug.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                nom_jug.setLayoutParams(lparams);
+
+                TextView intents_jug = new TextView(this);
+                intents_jug.setTextSize(20);
+                intents_jug.setText(Jugador.jugadors.get(i).getIntentos() + "");
+                intents_jug.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                intents_jug.setLayoutParams(lparams);
+
+                linearLayout.addView(nom_jug);
+                linearLayout.addView(intents_jug);
+                vertic.addView(linearLayout);
             }
-        });
-
-        //Coloca los jugadores que existan en la tabla de records
-        for (int i=0; i < Jugador.jugadors.size(); i++){
-            LinearLayout linearLayout = new LinearLayout(this);
-            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-
-            LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(0,100);
-            lparams.weight = 1;
-
-            TextView nom_jug = new TextView(this);
-            nom_jug.setTextSize(20);
-            nom_jug.setText(Jugador.jugadors.get(i).getNombre());
-            nom_jug.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            nom_jug.setLayoutParams(lparams);
-
-            TextView intents_jug = new TextView(this);
-            intents_jug.setTextSize(20);
-            intents_jug.setText(Jugador.jugadors.get(i).getIntentos()+"");
-            intents_jug.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            intents_jug.setLayoutParams(lparams);
-
-            linearLayout.addView(nom_jug);
-            linearLayout.addView(intents_jug);
-            vertic.addView(linearLayout);
         }
     }
 
     @Override
     public void onBackPressed(){
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
+        Intent intVR = getIntent();
+        if (intVR.getBooleanExtra("VER_RECORDS", false) == true){
+            finish();
+        } else {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.putExtra("EXTRA_DATOS", true);
+            startActivity(intent);
+        }
+    }
+
+    private void addRecordToFile(String nombre, String intentos){
+        File f = new File(getFilesDir().getPath()+"/recordsAndrevina.txt");
+        try {
+            if (!f.exists()){
+                f.createNewFile();
+            }
+            FileOutputStream fos = new FileOutputStream(f, true);
+            OutputStreamWriter writer = new OutputStreamWriter(fos);
+            writer.append(nombre+":"+intentos+System.lineSeparator());
+            writer.close();
+            fos.flush();
+            fos.close();
+
+            Jugador jugador = new Jugador(nombre, Integer.parseInt(intentos));
+            Jugador.jugadors.add(jugador);
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/andrevina/et/andrevina/MainActivity.java
+++ app/src/main/java/com/andrevina/et/andrevina/MainActivity.java
@@ -7,43 +7,56 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Window;
 import android.widget.Button;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
     private int intentos;
+    private boolean datosCargados = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        datosCargados = getIntent().getBooleanExtra("EXTRA_DATOS", false);
+        if (!datosCargados) {
+            getRecordsFromFile();
+        }
+
         intentos = 0;
         final int numero = (int) (Math.random()*100)+1;
         System.out.println("Pista: "+numero);//pista
         final Button button = findViewById(R.id.button);
-        final EditText eText = new EditText(this);
-        final TextView tv = (TextView) findViewById(R.id.numIntro);
-        tv.setOnKeyListener(new View.OnKeyListener() {
+        final TextView tvIntentos = findViewById(R.id.txtViewIntentos);
+        tvIntentos.setText("Intents: "+intentos);
+        final EditText numIntro = findViewById(R.id.numIntro);
+        numIntro.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (event.getAction() == KeyEvent.ACTION_DOWN){
                     if (keyCode == KeyEvent.KEYCODE_ENTER){
-                        System.out.println("click");
                         return true;
                     }
-
                 }
                 return false;
             }
         });
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String tvValue = tv.getText().toString();
+                String tvValue = numIntro.getText().toString();
                 final Context context = getApplicationContext();
                 CharSequence text = "";
                 int duration = Toast.LENGTH_LONG;
@@ -53,18 +66,21 @@ public class MainActivity extends AppCompatActivity {
                         text = "El numero es mes gran";
                         Toast toast = Toast.makeText(context, text, duration);
                         toast.show();
-                        tv.setText("");
+                        numIntro.setText("");
                         intentos++;
+                        tvIntentos.setText("Intents: "+intentos);
                     }
                     else if (num1 > numero){
                         text = "El numero es mes petit";
                         Toast toast = Toast.makeText(context, text, duration);
                         toast.show();
-                        tv.setText("");
+                        numIntro.setText("");
                         intentos++;
+                        tvIntentos.setText("Intents: "+intentos);
                     }
                     else if (num1 == numero) {
                         intentos++;
+                        tvIntentos.setText("Intents: "+intentos);
                         text = "Has encertat!";
                         Toast toast = Toast.makeText(context, text, duration);
                         toast.show();
@@ -107,7 +123,42 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        ImageView img = findViewById(R.id.verRecords);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FameActivity.class);
+                intent.putExtra("VER_RECORDS", true);
+                startActivity(intent);
+            }
+        });
     }
+
+    private void getRecordsFromFile(){
+        File f = new File(getFilesDir().getPath()+"/recordsAndrevina.txt");
+        try{
+            if (f.exists()){
+                FileInputStream fis = new FileInputStream(f);
+                BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+
+                String linea = "";
+                while ((linea = br.readLine()) != null) {
+                    if (linea.split(":").length == 2) {
+                        String[] player = linea.split(":");
+                        Jugador.jugadors.add(new Jugador(player[0], Integer.parseInt(player[1])));
+                    }
+                }
+                br.close();
+                fis.close();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
-    public void onBackPressed(){}
+    public void onBackPressed(){
+        moveTaskToBack(true);
+    }
 }
